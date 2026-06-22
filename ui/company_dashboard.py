b6eeedfb2f6f873cab6d2c280decf72ecbc7042b
shiranran-cpu <2218766362@qq.com>
2026-06-22 16:58:41 +0800

    Create company_dashboard.py

---
+++ ui/company_dashboard.py
@@ -0,0 +1,18 @@
+employees = [
+    {
+        "name": "PM",
+        "status": "Working"
+    },
+    {
+        "name": "Designer",
+        "status": "Working"
+    },
+    {
+        "name": "Frontend",
+        "status": "Done"
+    },
+    {
+        "name": "Backend",
+        "status": "Done"
+    }
+]
